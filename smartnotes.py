873af9757c373d00299fcb8893435c1807f39d5e
Rickard Lindberg <ricli85@gmail.com>
2020-09-26 14:15:14 +0200

    Animation of a box.

--- smartnotes.py
+++ smartnotes.py
@@ -7,12 +7,17 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((1280, 720))
     clock = pygame.time.Clock()
+    box = pygame.Surface((20, 20))
+    box.fill((123, 214, 55))
+    box_rect = box.get_rect()
     while True:
         print(f"frame = {clock.get_time()} (fps = {clock.get_fps()})")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        box_rect = box_rect.move((1, 1))
         screen.fill((100, 200, 50))
+        screen.blit(box, box_rect.topleft)
         pygame.display.flip()
         clock.tick(60)
 
