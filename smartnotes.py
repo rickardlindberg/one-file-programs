b63576df12fcb1e275ba6982cd20565a7e6c68d2
Rickard Lindberg <ricli85@gmail.com>
2020-09-26 19:23:01 +0200

    Clean up animation interface.

--- smartnotes.py
+++ smartnotes.py
@@ -33,23 +33,21 @@ class DebugBar(object):
         self.animation.start(200)
 
     def render(self, screen, ms_diff):
-        if not self.visible and self.animation.is_finished():
+        if not self.visible and not self.animation.active():
             return
         bar = pygame.Surface((screen.get_width(), self.HEIGHT))
         bar.fill((100, 100, 100))
         text, text_rect = self.font.render(
             f"ms_diff = {ms_diff} | fps = {int(round(self.clock.get_fps()))}"
         )
-        offset = 0
-        if not self.animation.is_finished():
-            percent = self.animation.advance(ms_diff)
-            if self.visible:
-                alpha = int(255 * percent)
-                offset = bar.get_height()-int(bar.get_height()*percent)
-            else:
-                alpha = 255 - int(255 * percent)
-                offset = int(bar.get_height()*percent)
-            bar.set_alpha(alpha)
+        percent = self.animation.advance(ms_diff)
+        if self.visible:
+            alpha = int(255 * percent)
+            offset = bar.get_height()-int(bar.get_height()*percent)
+        else:
+            alpha = 255 - int(255 * percent)
+            offset = int(bar.get_height()*percent)
+        bar.set_alpha(alpha)
         bar.blit(
             text,
             (
@@ -61,25 +59,25 @@ class DebugBar(object):
 class Animation(object):
 
     def __init__(self):
-        self.duration_ms = 0
-        self.progress = 0
-        self.is_active = False
-
-    def is_finished(self):
-        return self.progress == self.duration_ms and not self.is_active
+        self.duration_ms = 1
+        self.progress = 1
+        self.last_consumed = True
 
     def start(self, duration_ms):
         self.duration_ms = duration_ms
         self.progress = 0
-        self.is_active = True
+        self.last_consumed = False
 
     def advance(self, ms):
         percent = float(self.progress) / float(self.duration_ms)
         if self.progress == self.duration_ms:
-            self.is_active = False
+            self.last_consumed = True
         else:
             self.progress = min(self.duration_ms, self.progress+ms)
         return percent
+
+    def active(self):
+        return self.progress < self.duration_ms or not self.last_consumed
 
 def main():
     pygame.init()
