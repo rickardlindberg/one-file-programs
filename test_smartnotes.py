0b010d1372ff7f3e68e143e6c2db7161e8c74107
Rickard Lindberg <rickard@rickardlindberg.me>
2021-03-14 19:50:02 +0100

    We want automated tests: add basic skeleton.

---
+++ test_smartnotes.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import unittest
+
+class TestSmartNotes(unittest.TestCase):
+
+    def test_can_import_smart_notes(self):
+        import smartnotes
+
+if __name__ == "__main__":
+    unittest.main()
