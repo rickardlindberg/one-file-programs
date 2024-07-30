7eb08d25584f53d9f924ddc8cb2fce36ade484f6
Rickard Lindberg <rickard@rickardlindberg.me>
2024-07-30 19:11:12 +0200

    More sophisticated splitting of canvas.

--- no-scrollbars/noscrollbars.py
+++ no-scrollbars/noscrollbars.py
@@ -1,33 +1,26 @@
 import sys
+import random
+
+CLIP_LENGHTS = [random.randint(10, 25*60) for x in range(40)]
 
 class App:
-
-    def __init__(self):
-        self.deflate_factor = AnimatedValue(10, 100, 0.05)
 
     def event(self, event):
         if event.key_down("ctrl+q"):
             sys.exit(0)
 
     def update(self, elapsed_ms):
-        self.deflate_factor.update(elapsed_ms)
+        pass
 
     def draw(self, canvas):
         canvas.fill(color=(255, 0, 100))
-        canvas.with_rectangle(lambda rectangle: rectangle.deflate(self.deflate_factor.get())).stroke(color=(25, 25, 25))
-
-class AnimatedValue:
-
-    def __init__(self, start, end, speed):
-        self.start = start
-        self.end = end
-        self.speed = speed
-        self.value = start
-
-    def update(self, elapsed_ms):
-        self.value += elapsed_ms*self.speed
-        if self.value > self.end or self.value < self.start:
-            self.speed *= -1
-
-    def get(self):
-        return self.value
+        main = canvas.deflate(40)
+        main.stroke(color=(25, 25, 25))
+        main.columns([
+            {
+                "fn": lambda canvas: canvas.stroke(color=(20, 20, 100)),
+                "proportion": length,
+            }
+            for length
+            in CLIP_LENGHTS
+        ])
