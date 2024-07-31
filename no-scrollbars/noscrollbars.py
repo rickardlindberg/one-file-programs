4169d6c852f13e981d4df56eba0245cb2366117c
Rickard Lindberg <rickard@rickardlindberg.me>
2024-07-31 07:40:02 +0200

    Extract Clip.

--- no-scrollbars/noscrollbars.py
+++ no-scrollbars/noscrollbars.py
@@ -1,12 +1,11 @@
 import sys
 import random
 
-CLIP_LENGHTS = [random.randint(10, 25*60) for x in range(40)]
-
 class App:
 
     def __init__(self):
         self.main_rectangle = None
+        self.clips = [Clip(random.randint(10, 25*60)) for x in range(40)]
 
     def event(self, event):
         if event.key_down("ctrl+q"):
@@ -23,10 +22,21 @@ class App:
         main.stroke(color=(25, 25, 25))
         main.columns([
             {
-                "fn": lambda canvas: canvas.stroke(color=(20, 20, 100)),
-                "proportion": length,
+                "fn": clip.draw,
+                "proportion": clip.length*clip.magnification,
             }
-            for length
-            in CLIP_LENGHTS
+            for clip in self.clips
         ])
         self.main_rectangle = main.rectangle
+
+class Clip:
+
+    def __init__(self, length):
+        self.length = length
+        self.magnification = 1
+        self.rectangle = None
+
+    def draw(self, canvas):
+        canvas.stroke(color=(20, 20, 100))
+        canvas.text(str(self.length))
+        self.rectangle = canvas.rectangle
