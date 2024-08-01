6841b716bed7b08375232316edfbdc7a9a524292
Rickard Lindberg <rickard@rickardlindberg.me>
2024-08-01 08:28:30 +0200

    Clips to frames restart.

--- no-scrollbars/noscrollbars.py
+++ no-scrollbars/noscrollbars.py
@@ -4,64 +4,61 @@ import random
 class App:
 
     def __init__(self):
-        self.clips = Clips()
+        self.frames = Frames()
 
     def event(self, event):
         if event.key_down("ctrl+q"):
             sys.exit(0)
         else:
-            self.clips.event(event)
+            self.frames.event(event)
 
     def update(self, elapsed_ms):
         pass
 
     def draw(self, canvas):
         canvas.fill(color=(255, 0, 100))
-        self.clips.draw(canvas.deflate(40))
+        self.frames.draw(canvas.deflate(40))
 
-class Clips:
+class Frames:
 
     def __init__(self):
-        self.clips = [Clip(x) for x in range(50)]
+        self.frames = [Frame(x) for x in range(50)]
         self.rectangle = None
 
     def event(self, event):
-        if self.rectangle and event.mouse_motion():
-            sum_clip_lenghts = sum(clip.length for clip in self.clips)
-            frame_in_pixels = self.rectangle.width / sum_clip_lenghts
-            max_magnification = 200 / frame_in_pixels
-            for clip in self.clips:
-                foo = clip.rectangle.center.distance_to(event.mouse_point()).x
-                LIM = self.rectangle.width * 0.25
-                if foo < LIM:
-                    clip.magnification = max(1, max_magnification * ((LIM-foo)/LIM))
-                else:
-                    clip.magnification = 1
+        if event.mouse_motion():
+            print(self.find_position(event.mouse_point()))
 
     def draw(self, canvas):
         canvas.stroke(color=(25, 25, 25))
         canvas.columns([
             {
-                "fn": clip.draw,
-                "proportion": clip.length*clip.magnification,
+                "fn": frame.draw,
+                "proportion": 1,
             }
-            for clip in self.clips
+            for frame in self.frames
         ])
         self.rectangle = canvas.rectangle
 
-class Clip:
+    def find_position(self, mouse_point):
+        if self.rectangle:
+            positions = []
+            for frame in self.frames:
+                if frame.rectangle.contains(mouse_point):
+                    positions.append(
+                        frame.number + frame.rectangle.percent(mouse_point).x
+                    )
+            if positions:
+                return sum(positions) / len(positions)
+        return len(self.frames) / 2
 
-    def __init__(self, x):
-        self.length = 1
-        self.text = str(x)
-        self.magnification = 1
+class Frame:
+
+    def __init__(self, number):
+        self.number = number
         self.rectangle = None
 
     def draw(self, canvas):
         self.rectangle = canvas.rectangle
-        if self.magnification == 1:
-            sub = canvas.deflate(height=20)
-        else:
-            sub = canvas
-        sub.stroke(color=(20, 20, 100))
-        sub.text(self.text)
+        canvas.stroke(color=(20, 20, 100))
+        canvas.text(str(self.number))
