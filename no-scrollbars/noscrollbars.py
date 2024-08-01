55c90ec45f5756acf86179b765741b7fbd281ff6
Rickard Lindberg <rickard@rickardlindberg.me>
2024-08-01 09:59:36 +0200

    Clean up with reset.

--- no-scrollbars/noscrollbars.py
+++ no-scrollbars/noscrollbars.py
@@ -24,20 +24,22 @@ class Frames:
     def __init__(self):
         self.frames = [Frame(x) for x in range(50)]
         self.rectangle = None
-        self.position = self.find_position()
+        self.reset()
+
+    def reset(self):
+        self.position = len(self.frames) / 2
         self.number_of_frames_to_magnify = 5
         self.magnification_percent = 0.3
         self.deflation_base = 30
 
     def event(self, event):
         if event.mouse_motion():
-            self.position = self.find_position(event.mouse_point())
-            if self.rectangle.contains(event.mouse_point()):
+            if event.mouse_motion(inside=self.rectangle):
+                self.position = self.find_position(event.mouse_point())
                 percent_away_from_vertical_center = 2*abs(0.5 - self.rectangle.percent(event.mouse_point()).y)
                 self.magnification_percent = 1 * percent_away_from_vertical_center
             else:
-                self.number_of_frames_to_magnify = 5
-                self.magnification_percent = 0.3
+                self.reset()
 
     def update(self, elapsed_ms):
         self.before = []
@@ -88,16 +90,15 @@ class Frames:
     def draw_after(self, canvas):
         canvas.columns([{"fn": frame.draw} for frame in self.after])
 
-    def find_position(self, mouse_point=None):
-        if self.rectangle:
-            positions = []
-            for frame in self.frames:
-                if frame.rectangle.contains(mouse_point):
-                    positions.append(
-                        frame.number + frame.rectangle.percent(mouse_point).x
-                    )
-            if positions:
-                return sum(positions) / len(positions)
+    def find_position(self, mouse_point):
+        positions = []
+        for frame in self.frames:
+            if frame.rectangle.contains(mouse_point):
+                positions.append(
+                    frame.number + frame.rectangle.percent(mouse_point).x
+                )
+        if positions:
+            return sum(positions) / len(positions)
         return len(self.frames) / 2
 
 class Frame:
